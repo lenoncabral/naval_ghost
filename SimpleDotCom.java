8dcf447c5e03f5b94341a920d2797e370dd9dae2
lenoncabral <lenon.cabral@hotmail.com>
2013-03-29 00:33:51 -0300

    4

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -10,10 +10,10 @@ public class SimpleDotCom {
 	
 	public String checkYourself (String stringGuess){
 	int guess = Integer.parseInt (stringGuess);
-	String result = "miss";
+	String result = "Errou!";
 	for (int cell : locationCells) {
 		if (guess == cell) {
-			result = "hit";
+			result = "Belo Tiro!";
 			numOfHits++;
 			break;
 		}
@@ -21,7 +21,7 @@ public class SimpleDotCom {
 	
 	if (numOfHits ==
 	locationCells.length) {
-		result = "kill";
+		result = "Tango Down, He's Dead";
 	}
 	System.out.println(result);
 	return result;
